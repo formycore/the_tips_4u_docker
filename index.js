a40851076c6788ab5afe624d874d0d7345701dc6
formycore <formycore@gmail.com>
2022-07-04 23:18:09 +0530

    added

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-    res.send('{ "response": "Hello from Samantha" }');
+    res.send('{ "response": "Hello from Samantha and alia" }');
 });
 
 app.get('/will', function (req, res) {
